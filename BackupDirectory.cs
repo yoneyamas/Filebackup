c34cd67144ed274216702f99c02709a86445ae4a
yoneyamas <ynymsvbac@gmail.com>
2017-03-04 03:20:01 +0900

    画面レイアウトの作成

---
+++ BackupDirectory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filebackup {
+    class BackupDirectory {
+        private List<string> directoryList;
+
+        public BackupDirectory() {
+            directoryList = new List<string>();
+
+        }
+    }
+}
